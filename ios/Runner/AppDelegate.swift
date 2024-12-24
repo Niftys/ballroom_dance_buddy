d8e8f4c91f1e1088178b425224b51bef2860eda4
Niftys <lowery.seth8@gmail.com>
2024-12-24 13:02:06 -0600

    ios update

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,20 +1,17 @@
-import Flutter
 import UIKit
+import Flutter
+import flutter_inappwebview_ios
+import just_audio
+import file_picker
+import audio_session
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-
-
-    if #available(iOS 14.0, *) {
-      application.registerForRemoteNotifications()
-    }
-
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
-
